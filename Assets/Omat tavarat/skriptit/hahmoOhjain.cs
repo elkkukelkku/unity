dd48611fc04ad7cdbb62ad63d8fd6b0d6e900307
Your Name <you@example.com>
2020-11-26 17:37:03 +0200

    mitä tähän pitää kirjoittaa

---
+++ Assets/Omat tavarat/skriptit/hahmoOhjain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hahmoOhjain : MonoBehaviour
+{
+    public float juoksuNopeus = 3;
+    public float hiirenNopeus = 3;
+    public float painovoima = 10;
+    public float horisontaalinenPyorinta = 0;
+    public float vertikaalinenPyorinta = 0;
+    public float maxKaannosAsteet = 60;
+    public CursorLockMode haluttuMoodi;
+    private Vector3 liikesuunta = Vector3.zero;
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = haluttuMoodi;
+
+        Cursor.visible = (CursorLockMode.Locked != haluttuMoodi);
+    }
+
+    // Update kutsutaan kerran framessa
+    void Update()
+    {
+        horisontaalinenPyorinta += Input.GetAxis("Mouse X") *hiirenNopeus;
+        vertikaalinenPyorinta -= Input.GetAxis("Mouse Y") *hiirenNopeus;
+
+        //Debug.Log($"asteet {horisontaalinenPyorinta}");
+        vertikaalinenPyorinta = Mathf.Clamp(vertikaalinenPyorinta,-maxKaannosAsteet,maxKaannosAsteet);
+        transform.localRotation = Quaternion.Euler(vertikaalinenPyorinta,horisontaalinenPyorinta,0);
+    }
+}
